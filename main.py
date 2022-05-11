decc1ff19be80de837d53d2defba8ca069be16aa
Narmin <narmin@Narmins-MacBook-Air.local>
2022-05-11 14:59:13 +0400

    filter users functionality on users page

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from typing import List
 import uvicorn
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from users import models
 from users.database import engine, SessionLocal
-from users.crud import get_user, get_users
+from users.crud import get_user, filter_users, get_users
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -39,6 +41,52 @@ def read_volunteers(skip: int = 0, limit: int = 100, db: Session = Depends(get_d
 def read_volunteer(user_id: int, db: Session = Depends(get_db)):
     user = get_user(db=db, user_id=user_id)
     return user    
+
+
+class User(BaseModel):
+    field: str
+    operator: str
+    value: str
+
+class UserList(BaseModel):
+    users: List[User]
+
+@app.post('/filter-volunteers/')
+def filter_volunteers(data: UserList, db: Session = Depends(get_db)):
+    matched_users = []
+    print(data.users, 'query over here..........')
+    for i in data.users: 
+        print(i, 'iiiii')
+        print(i.field)
+        field = i.field
+        operator = i.operator
+        value = i.value
+        print(type(value), 'value type')
+        print(field, 'field', operator, 'operator', value, 'value')
+        user = filter_users(db, field=field, operator=operator, value=value)
+        if user not in matched_users:
+            matched_users.append(user)
+            print(matched_users, "all matching users")
+    return matched_users
+
+
+# [
+# {
+# field: "name",
+# operator: "=",
+# value: "Musa",
+# },
+# {
+# field: "age",
+# operator: ">",
+# value: "22",
+# },
+# {
+# field: "location",
+# operator: "contains",
+# value: "B",
+# }
+# ]
 
 
 if __name__ == "__main__":
--- users/crud.py
+++ users/crud.py
@@ -8,3 +8,26 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 def get_user(db: Session, user_id: int):
     return db.query(models.Volunteers).filter(models.Volunteers.id == user_id).first()
+
+
+def filter_users(db: Session, field, operator, value):
+    print(field,operator, value, 'query get user filter func')
+
+    if operator == '=':
+        print('operator is =')
+        return db.query(models.Volunteers).filter(getattr(models.Volunteers, field) == value).first()
+    elif operator == '>':
+        print('operator is >')
+        return db.query(models.Volunteers).filter(getattr(models.Volunteers, field) > value).first()
+    elif operator == '<':
+        print('operator is <')
+        return db.query(models.Volunteers).filter(getattr(models.Volunteers, field) < value).first()
+    elif operator == '>=':
+        print('operator is >=')
+        return db.query(models.Volunteers).filter(getattr(models.Volunteers, field) >= value).first()
+    elif operator == '<=':
+        print('operator is <=')
+        return db.query(models.Volunteers).filter(getattr(models.Volunteers, field) <= value).first()
+    elif operator == 'contains':
+        print('operator is contains')
+        return db.query(models.Volunteers).filter(getattr(models.Volunteers, field).contains(value)).first()
